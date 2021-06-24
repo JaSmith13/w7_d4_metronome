40320cdf649e272cfb27d8508b6a5c3cf793fe11
Alastair Smith <j.alastair.smith@outlook.com>
2021-06-24 11:37:34 +0100

    adds play buttton to toggle state

--- src/containers/ControlBox.js
+++ src/containers/ControlBox.js
@@ -1,15 +1,21 @@
 import {useState} from 'react'
 
 const ControlBox = ({BPM, changeBPM}) => {
+    
+    const [start, setStart] = useState(false)
 
     const handleChange = (event) => {
         changeBPM(event.target.value)
     }
 
+    const handleClick = () => {
+        setStart(!start) 
+    }
 
     return(
         <div>
             <input type="range" id='BPM' name='BPM' min='40' max='220' onChange={handleChange} />
+            <button onClick={handleClick} >Play</button>
         </div>
     )
 }
